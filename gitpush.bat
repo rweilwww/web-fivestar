7b24b1abc860bc72ac5ba81e8c56dc916a749fc2
rweil55 <github@shaw-weil.com>
2024-01-12 09:31:44 -0500

    from indesign 2024-01-12 14:29

--- gitpush.bat
+++ gitpush.bat
@@ -1,5 +1,5 @@
 
 cd /inetpub/wwwroot/w/fivestar
 git add .
-git commit -m "from indesign 2023-12-31 10:22" --no-edit
+git commit -m "from indesign 2024-01-12 14:29" --no-edit
 git push https://github.com/rweil55/fivestar
