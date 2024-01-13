079ea4541354184e25ec0d29baead5c050ba586e
rweil55 <github@shaw-weil.com>
2024-01-12 20:56:08 -0500

    from indesign 2024-01-13 01:55

--- gitpush.bat
+++ gitpush.bat
@@ -1,9 +1,9 @@
 
 cd /inetpub/wwwroot/w/fivestar
 git add .
-git commit -m "from indesign 2024-01-13 01:51" --no-edit
+git commit -m "from indesign 2024-01-13 01:55" --no-edit
 git push https://github.com/rweil55/web-fivestar
 
 rem wait for one minute ro allow git hub to get irs act together
-slepp 60
+timeout 60
 rem go fo it!!
