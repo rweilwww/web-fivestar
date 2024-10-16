ba4a1ead60960ebc19822a2ea314d8adbffe8692
rweil55 <github@shaw-weil.com>
2024-10-15 20:16:54 -0400

    from indesign 2024-10-16 00:16

--- gitpush.bat
+++ gitpush.bat
@@ -3,7 +3,7 @@ cd /inetpub/wwwroot/w/fivestar
 
 
 git add .
-git commit -m "from indesign 2024-10-16 00:12" --no-edit
+git commit -m "from indesign 2024-10-16 00:16" --no-edit
 git push https://github.com/rweil55/web-fivestar main
 
 rem wait for one minute to allow git hub to get its act together
