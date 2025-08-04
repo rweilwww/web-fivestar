2c4a96253f5043ff8ebec68b2391ea5cb7fa3267
rweilwww <github@shaw-weil.com>
2025-08-04 12:17:21 -0400

    from indesign 2025-08-04 16:16

--- gitpush.bat
+++ gitpush.bat
@@ -3,8 +3,8 @@ cd /inetpub/wwwroot/w/fivestar
 
 
 git add .
-git commit -m "from indesign 2024-12-24 01:30" --no-edit
-git push https://github.com/rweil55/web-fivestar main
+git commit -m "from indesign 2025-08-04 16:16" --no-edit
+git push --force --set-upstream origin main
 
 rem wait for one minute to allow git hub to get its act together
 timeout 60
